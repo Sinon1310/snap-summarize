57fb76ed7bd93ed7ebe69a373b50866d35fb33dd
sinon <rodriguessinon@gmail.com>
2026-02-28 09:23:28 +0530

    Initial commit

---
+++ test/snap-summarize.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SnapSummarize from '../lib/snap-summarize-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/snap-summarize-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SnapSummarize.SnapSummarizeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
